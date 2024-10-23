0907919e242d9e07ec08411aabaac92d38dc0184
ArturoRoncal2704 <roncalhermenegildo@gmail.com>
2024-10-23 02:25:46 -0500

    Añadiendo las entidades

--- src/main/java/com/example/demo/repository/EstudianteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.EstudianteEntity;
-
-public interface EstudianteRepository extends JpaRepository<EstudianteEntity, String>{
-
-}
